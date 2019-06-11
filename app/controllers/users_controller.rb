c8080a3e1541b753ff6ff58005409d06d2b2020a
Anna Lornitzo <lornitzoa@gmail.com>
2019-06-11 17:15:27 -0400

    basic rails authorization set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+  before_action :authenticate_token, except: [:login, :create]
+  before_action :authorize_user, except: [:login, :create, :index]
 
   # GET /users
   def index
@@ -10,7 +12,12 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
-    render json: @user
+    render json: get_current_user
+  end
+
+  # Authorize user
+  def authorize_user
+    render json: {status: 401, message: "Unauthorized"} unless get_current_user.id == params[:id].to_i
   end
 
   # POST /users
@@ -38,6 +45,16 @@ class UsersController < ApplicationController
     @user.destroy
   end
 
+  def login
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      token = create_token(user.id, user.username)
+      render json: {status: 200, token: token, user: user}
+    else
+      render json: {status: 401, message: "Unauthorized"}
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
@@ -48,4 +65,23 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:username, :password_digest)
     end
+
+    def create_token(id, username)
+      JWT.encode(payload(id, username), ENV['JWT_SECRET'], 'HS256')
+    end
+
+    # Return hash payload with users's id and username to be encrypted
+    def payload(id, username)
+      {
+        exp: (Time.now + 30.minutes).to_i,
+        iat: Time.now.to_i,
+        iss: ENV['JWT_ISSUER'],
+        user: {
+          id: id,
+          usernam: username
+        }
+      }
+    end
+
+
 end
